4c33c00bfce3e8bdb14914cb9fe4369b0f533632
Reasno <guxi99@gmail.com>
2021-07-29 13:22:03 +0800

    refactor: move factory to its own file

--- dependency.go
+++ dependency.go
@@ -43,44 +43,10 @@ func Providers() di.Deps {
 // Gauge is an alias used for dependency injection
 type Gauge metrics.Gauge
 
-// ConsumableDispatcher is the key JobFrom *Queue in the dependencies graph. Used as a type hint for injection.
+// ConsumableDispatcher is the key of *Queue in the dependencies graph. Used as a type hint for injection.
 type ConsumableDispatcher interface {
 	JobDispatcher
 	Consume(ctx context.Context) error
-}
-
-// DispatcherFactory is a factory for *Queue. Note DispatcherFactory doesn't contain the factory method
-// itself. ie. How to factory a dispatcher left there for users to define. Users then can use this type to create
-// their own dispatcher implementation.
-//
-// Here is an example on how to create a custom DispatcherFactory with an InProcessDriver.
-//
-//		factory := di.NewFactory(func(name string) (di.Pair, error) {
-//			queuedDispatcher := queue.NewQueue(
-//				&Jobs.SyncDispatcher{},
-//				queue.NewInProcessDriver(),
-//			)
-//			return di.Pair{Conn: queuedDispatcher}, nil
-//		})
-//		dispatcherFactory := DispatcherFactory{Factory: factory}
-//
-type DispatcherFactory struct {
-	*di.Factory
-}
-
-// Make returns a Queue by the given name. If it has already been created under the same name,
-// the that one will be returned.
-func (s DispatcherFactory) Make(name string) (*Queue, error) {
-	client, err := s.Factory.Make(name)
-	if err != nil {
-		return nil, err
-	}
-	return client.(*Queue), nil
-}
-
-// DispatcherMaker is the key JobFrom *DispatcherFactory in the dependencies graph. Used as a type hint for injection.
-type DispatcherMaker interface {
-	Make(string) (*Queue, error)
 }
 
 type configuration struct {
